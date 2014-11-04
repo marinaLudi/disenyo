362c8eda3cffa90f87cfaa018b08a75ed9007717
argot42 <pablotesari@gmail.com>
2014-11-04 18:12:16 -0300

    adicion de dtopasajero

---
+++ La_ribera_manager/La_ribera_manager_app/objetos/dtopasajero.py
@@ -0,0 +1,23 @@
+class DtoPasajero:
+	def __init__(self, nombre=None, apellido=None, cuit=None, email=None, fecha_de_nac=None, telefono=None, id_ocupacion=None, descripcion=None, nombrePais=None, id_nacionalidad=None, tipo=None, codigo=None, numero=None, calle=None, piso=None, dpto=None, nombreLocalidad=None, id_localidad=None, nombreProv=None, id_prov=None, id_pais=None):
+		self.nombre = nombre
+		self.apellido = apellido
+		self.cuit = cuit
+		self.email = email
+		self.fecha_de_nac = fecha_de_nac
+		self.telefono = telefono
+		self.id_ocupacion = id_ocupacion
+		self.descripcion = descripcion
+		self.nombrePais = nombrePais
+		self.id_nacionalidad = id_nacionalidad
+		self.tipo = tipo
+		self.codigo = codigo
+		self.numero = numero
+		self.calle = calle
+		self.piso = piso
+		self.dpto = dpto
+		self.nombreLocalidad = nombreLocalidad
+		self.id_localidad = id_localidad
+		self.nombreProv = nombreProv
+		self.id_prov = id_prov
+		self.id_pais = id_pais
