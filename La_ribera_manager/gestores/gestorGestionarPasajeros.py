ae35fe0b173515a6dc8d24f9e707327812a64c2c
argot42 <pablotesari@gmail.com>
2014-11-05 01:45:38 -0300

    gestor de pasajeros y adiciondel directorio padre al path

--- La_ribera_manager/gestores/gestorGestionarPasajeros.py
+++ La_ribera_manager/gestores/gestorGestionarPasajeros.py
@@ -1,1 +1,17 @@
-from La_ribera_manager_app.objetos.pasajero import Pasajero
+#### Gestor Gestionar Pasajeros ####
+
+import sys, os, inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from objetos.pasajero import Pasajero
+from db.gestordb import GestorDB
+
+class GestorGestionarPasajeros:
+	def buscar(self, nombre, apellido, tipoDocu, Documento):
+		database = GestorDB()
+
+		return database.buscarPasajero(nombre, apellido, tipoDocu, Documento)
+
