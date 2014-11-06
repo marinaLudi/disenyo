8c187488b870251db74fe87eecf6783227710576
marina <ludimarina@gmail.com>
2014-11-06 00:35:07 -0300

    termine de agregar los handlers

--- La_ribera_manager/interfazPasajero.py
+++ La_ribera_manager/interfazPasajero.py
@@ -1,18 +1,28 @@
 from gi.repository import Gtk
 from sqlalchemy import *
 import datetime
+from objetos.dtopasajero import DtoPasajero
 
 class interfazPasajero:
 	def __init__(self):
 		
 		builder = Gtk.Builder()
-		builder.add_from_file("Cargar_Pasajero3.4.xml")
+		builder.add_from_file("interfaces/Cargar_Pasajero3.4.xml")
 		
 		self.window1 = builder.get_object("window1")
 		self.bSiguiente = builder.get_object("bSiguiente")
 		self.eNombres = builder.get_object("eNombres")
 		self.eApellidos = builder.get_object("eApellidos")
+		self.eDocumento = builder.get_object("eDocumento")
+		self.eTelefono = builder.get_object("eTelefono")
+		self.eDireccion = builder.get_object("eDireccion")
+		self.eDepto = builder.get_object("eDepto")
+		self.ePiso = builder.get_object("ePiso")
+		self.ePostal = builder.get_object("ePostal")
+		self.eCUIT = builder.get_object("eCUIT")
+		self.eIVA = builder.get_object("eIVA")
 		self.cPais = builder.get_object("cPais")
+		self.eCorreo = builder.get_object("eCorreo")
 		self.cProvincia = builder.get_object("cProvincia")
 		self.cLocalidad = builder.get_object("cLocalidad")
 		self.cDocumento = builder.get_object("cDocumento")
@@ -26,19 +36,51 @@ class interfazPasajero:
 		self.cargarCombos(self.cPais,self.cProvincia,self.cLocalidad,self.cDocumento,self.cDia,self.cMes,self.cAnyo,self.cNacionalidad,self.cOcupacion,)
 		
 		handlers = {
-		"on_bSiguiente_clicked": "",
+		"on_bSiguiente_clicked": self.on_bSiguiente_clicked,
 		"on_window1_destroy": Gtk.main_quit,
-		"on_cPais_changed": "",
-		"on_cProvincia_changed": "",
-		"on_cLocalidad_changed": "",
-		"on_cDocumento_changed": "",
-		"on_cNacionalidad_changed": "",
-		"on_cAnyo_changed": "",
-		"on_cDia_changed": "",
+		"on_cPais_changed": self.on_cPais_changed,
+		"on_cProvincia_changed": self.on_cProvincia_changed,
+		"on_cLocalidad_changed": self.on_cLocalidad_changed,
+		"on_cDocumento_changed": self.on_cDocumento_changed,
+		"on_cNacionalidad_changed": self.on_cNacionalidad_changed,
+		"on_cAnyo_changed": self.on_cAnyo_changed,
+		"on_cMes_changed": self.on_cMes_changed,
+		"on_cDia_changed": self.on_cDia_changed,
+		"on_cOcupacion_changed":self.on_cOcupacion_changed
 		}
 		builder.connect_signals(handlers)
 		
 		self.window1.show_all()
+		
+	def on_cDocumento_changed(self,combo):
+		print "sss"	
+		
+	def on_cOcupacion_changed(self,combo):
+		print "sss"
+		
+	def on_cNacionalidad_changed(self,combo):
+		print "sss"	
+	
+	def on_cAnyo_changed(self,combo):
+		print "sss"	
+	
+	def on_cDia_changed(self,combo):
+		print "sss"	
+				
+	def on_cLocalidad_changed(self,combo):
+		print "sss"	
+	
+	def on_cMes_changed(self,combo):
+		print "sss"	
+		
+	def on_bSiguiente_clicked(self,boton):#ver!
+		pasajero = DtoPasajero(self.eNombres.get_text(),self.eApellidos.get_text(),self.eCUIT.get_text(),self.eCorreo.get_text(),None,self.eTelefono.get_text(),None,None,self.eDireccion.get_text(),self.eDepto.get_text(),self.ePiso.get_text(),self.ePostal.get_text(),self.eIVA.get_text())
+		print pasajero.nombre
+		
+	def on_cPais_changed(self,combo):
+		print "pais"
+	def on_cProvincia_changed(self,combo):
+		print "provincia"
 	
 	
 	def cargarCombos(self,cPais,cProvincia,cLocalidad,cDocumento,cDia,cMes,cAnyo,cNacionalidad,cOcupacion):
